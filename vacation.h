507fb00a047efd5a028337207196fa1d147b1b55
Ilan Smith <ilansm@nvidia.com>
2024-10-17 00:17:30 +0300

    Separate argument parsing from main function
    Signed-off-by: Ilan Smith <ilansm@nvidia.com>

---
+++ vacation.h
@@ -0,0 +1,25 @@
+#ifndef _VACATION_H_
+#define _VACATION_H_
+
+#include <stdint.h>
+
+struct vacation_params {
+	uint16_t vacation_days_per_year;
+	uint16_t max_vacation_accumulation_allowed;
+	double payslip_vacation_hours;
+};
+
+struct vacation_stats {
+	uint16_t days_due_total;
+	uint16_t days_due_this_year;
+	uint16_t days_remaining_this_year;
+	uint16_t days_due_accumulated;
+	uint16_t total_due_at_end_of_year;
+	uint16_t expected_excess_at_end_of_year;
+};
+
+int vacation_calculate(struct vacation_params *params,
+	struct vacation_stats *stats);
+
+#endif
+
